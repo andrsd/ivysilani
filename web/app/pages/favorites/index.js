488264f196b3056cc917afaf337a9c30db11a9d9
David Andrs <andrsd@gmail.com>
2020-06-07 08:23:23 -0600

    Adding context menu to Favorites page
    Closes #10

--- web/app/pages/favorites/index.js
+++ web/app/pages/favorites/index.js
@@ -1,12 +1,16 @@
 import ATV from 'atvjs'
 import fastXmlParser from 'fast-xml-parser'
 import template from './template.hbs'
+import context_menu from './context-menu.hbs'
 import API from 'lib/ivysilani.js'
 import Favorites from 'lib/favorites.js'
 
 const FavoritesPage = ATV.Page.create({
   name: 'favorites',
   template,
+  events: {
+    holdselect: 'onHoldSelect'
+  },
   ready (options, resolve, reject) {
     let favs = Favorites.get()
 
@@ -40,7 +44,28 @@ const FavoritesPage = ATV.Page.create({
     else {
       resolve()
     }
-  }
+  },
+  onHoldSelect(e) {
+    let element = e.target
+
+    if (element.nodeName === 'lockup') {
+      var programme = JSON.parse(element.getAttribute("data-href-page-options"))
+
+      var doc = ATV.Navigation.presentModal({
+        template: context_menu,
+        data: {
+          programme: programme
+        }
+      })
+
+      doc
+        .getElementById('favorite-btn')
+        .addEventListener('select', () => {
+          ATV.Navigation.dismissModal()
+          Favorites.remove(programme.ID)
+        })
+    }
+  },
 })
 
 export default FavoritesPage
