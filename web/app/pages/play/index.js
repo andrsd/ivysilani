3f9c6a3f9bbdec2a010e23d634a122e6fdd6ed2e
David Andrs <andrsd@gmail.com>
2020-06-27 19:51:29 -0600

    Report an error if show is not playable
    Refs #14

--- web/app/pages/play/index.js
+++ web/app/pages/play/index.js
@@ -33,58 +33,69 @@ const PlayPage = ATV.Page.create({
       .all([getPlaylistUrl])
       .then((xhrs) => {
         const parsed = fastXmlParser.parse(xhrs[0].response)
-        const playlistUrl = Object.values(parsed)[0]
-        console.log(playlistUrl)
+        if ('playlistURL' in parsed) {
+          const playlistUrl = Object.values(parsed)[0]
+          console.log(playlistUrl)
 
-        const tvosPlaylist = new Playlist()
+          const tvosPlaylist = new Playlist()
 
-        const playlist = API.syncAjax(playlistUrl, { responseType: 'json' }).playlist
+          const playlist = API.syncAjax(playlistUrl, { responseType: 'json' }).playlist
 
-        Object.entries(playlist).forEach(([key, value]) => {
-          const mediaItem = new MediaItem('video', value.streamUrls.main)
-          mediaItem.artworkImageURL = value.previewImageUrl
-          mediaItem.title = value.title
-          tvosPlaylist.push(mediaItem)
+          Object.entries(playlist).forEach(([key, value]) => {
+            const mediaItem = new MediaItem('video', value.streamUrls.main)
+            mediaItem.artworkImageURL = value.previewImageUrl
+            mediaItem.title = value.title
+            tvosPlaylist.push(mediaItem)
 
-          if (key == 0) {
-            player.playlist = tvosPlaylist
+            if (key == 0) {
+              player.playlist = tvosPlaylist
 
-            player.addEventListener('timeDidChange', function(info) {
-              History.setProgressTime(programme_id, info.time)
-            }, {
-              interval: 1
-            })
-          }
-        })
-
-        var watched = History.watched(programme_id)
-
-        if ((watched > 0) && (watched < 0.99)) {
-          var progressTime = History.progressTime(programme_id)
-          var time = new Date(progressTime * 1000).toISOString('H:mm:ss').substr(11, 8)
-
-          var doc = ATV.Navigation.presentModal({
-            template: template,
-            data: {
-              time: time
-            },
+              player.addEventListener('timeDidChange', function(info) {
+                History.setProgressTime(programme_id, info.time)
+              }, {
+                interval: 1
+              })
+            }
           })
-          doc
-            .getElementById('play-btn')
-            .addEventListener('select', () => {
-              player.play()
-            })
 
-          doc
-            .getElementById('resume-btn')
-            .addEventListener('select', () => {
-              player.seekToTime(progressTime)
-              player.play()
+          var watched = History.watched(programme_id)
+
+          if ((watched > 0) && (watched < 0.99)) {
+            var progressTime = History.progressTime(programme_id)
+            var time = new Date(progressTime * 1000).toISOString('H:mm:ss').substr(11, 8)
+
+            var doc = ATV.Navigation.presentModal({
+              template: template,
+              data: {
+                time: time
+              },
             })
+            doc
+              .getElementById('play-btn')
+              .addEventListener('select', () => {
+                player.play()
+              })
+
+            doc
+              .getElementById('resume-btn')
+              .addEventListener('select', () => {
+                player.seekToTime(progressTime)
+                player.play()
+              })
+          }
+          else {
+            player.play()
+            resolve(false)
+          }
         }
         else {
-          player.play()
-          resolve(false)
+          ATV.Navigation.showError({
+            data: {
+              title: 'Chyba',
+              message: 'Video se nepodařilo nalézt'
+            },
+            type: 'document'
+          })
         }
       }, (xhr) => {
         reject(xhr)
